4482952ecad9440aa8588d23cb577d72281a58f3
Joseph Tubridy <jftubridy@gmail.com>
2019-07-19 08:19:44 -0700

    add basic starting layout

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace BakedGoods {
+
+    public class Bread
+        {
+        public static void Main()
+            {
+
+            }
+        }
+
+    public class Pastry
+        {
+        public static void Main()
+            {
+
+            }
+        }
+
+}
